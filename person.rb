1b1116d95b61b286f9557558a86e695d0c04ddfc
yosefHesham <youssef.h@trufla.com>
2022-11-04 17:13:08 +0200

    Fix linters

--- person.rb
+++ person.rb
@@ -1,5 +1,5 @@
 class Person
-  def initialize(age, name = 'Unkown', _parent_permission = true)
+  def initialize(age, name = 'Unkown', _parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
@@ -9,12 +9,12 @@ class Person
   attr_accessor :name, :age
 
   def can_use_services?
-    is_of_age?
+    of_age?
   end
 
   private
 
-  def is_of_age?
+  def of_age?
     true if @age >= 18
     false
   end
