781d2b38684087e8b4eb596d55aa6282d69bbf15
yosefHesham <youssef.h@trufla.com>
2023-02-01 00:38:16 +0200

    Create trimmer

---
+++ trimmer.rb
@@ -0,0 +1,14 @@
+class CapitalizeDecorator < BaseDecorator
+  def initialize(nameable)
+    @nameable = nameable
+    super(nameable)
+  end
+
+  def correct_name
+    if @nameable.correct_name.size > 10
+      @nameable.correct_name.slice(0..11)
+    else
+      @nameable.correct_name
+    end
+  end
+end
