15e197ba15b9f9dbd911f2f7a6068c078463023c
yosefHesham <youssef.h@trufla.com>
2023-02-01 22:40:38 +0200

    Create classrom/student relationship

--- student.rb
+++ student.rb
@@ -3,6 +3,7 @@ class Student < Person
   def initialize(age, classroom, name = 'Unkown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
+    classroom.students.push(self)
   end
 
   def play_hooky
